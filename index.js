7bc25ebc721602c2e033dab7a66a4fca44d284be
Takuya Matsuyama <nora@odoruinu.net>
2016-07-23 13:49:37 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import checkbox from 'markdown-it-checkbox';
+import { CompositeDisposable } from 'inkdrop';
+
+export default {
+  configDefaults: {},
+
+  activate () {
+    this.subscriptions = new CompositeDisposable();
+    this.subscriptions.add(
+      inkdrop.components.onComponentDidMount('MDEPreview', ::this.handlePreviewUpdate)
+    );
+    this.subscriptions.add(
+      inkdrop.components.onComponentDidUpdate('MDEPreview', ::this.handlePreviewUpdate)
+    );
+    let { MDEPreview } = inkdrop.components.classes;
+    return MDEPreview.renderer.use(checkbox, {
+      divWrap: true,
+      divClass: 'ui checkbox md-checkbox'
+    }
+    );
+  },
+
+  handlePreviewUpdate ({ node }) {
+    return $(node).find('.md-checkbox > input[type="checkbox"]').on('click', this.hookCheckboxClick);
+  },
+
+  hookCheckboxClick (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  },
+
+  deactivate () {
+    this.subscriptions.dispose();
+  }
+
+};
